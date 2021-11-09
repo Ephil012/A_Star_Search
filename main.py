6643a03f70c09c67596fe44d488ec3a2d663b200
Ephil012 <Etp5501@gmail.com>
2021-11-09 16:07:03 -0500

    Added ability to get file and weights

---
+++ main.py
@@ -0,0 +1,11 @@
+# Get file name from command line
+filename = input("Enter file name: ")
+
+# Open file
+f = open(filename, 'r')
+
+# Get weight from command line
+weight = input("Enter weight: ")
+# Convert weight to float
+weight = float(weight)
+
